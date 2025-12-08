3028f6e9fc0a09cf06e1d1f11a1d996a148164e3
trev <me@trev.xyz>
2025-12-08 03:54:17 -0500

    bump: v0.2.7 -> v0.2.8

--- flake.nix
+++ flake.nix
@@ -128,7 +128,7 @@
         packages = {
           default = pkgs.stdenv.mkDerivation (finalAttrs: {
             pname = "nix-flake-release";
-            version = "0.2.7";
+            version = "0.2.8";
 
             src = builtins.path {
               name = "root";
