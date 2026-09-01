20981e6170a5dea048dd17c7482aa54bbef94223
doctor <doctor@trev.xyz>
2026-09-01 22:06:05 +0000

    bump: v0.24.1 -> v0.24.2

--- flake.nix
+++ flake.nix
@@ -182,7 +182,7 @@
         packages.default = pkgs.buildGoModule (
           final: with pkgs.lib; {
             pname = "flake-release";
-            version = "0.24.1";
+            version = "0.24.2";
 
             src = fileset.toSource {
               root = ./.;
